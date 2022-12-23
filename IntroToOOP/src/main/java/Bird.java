b4de0cb7cecf4a0cde890e89c04dfd531993389e
VictoriaIvons <victoriaivons@yahoo.com>
2022-12-23 19:54:31 +0000

    Saved progress on IntroToOOP 2022-12-23 19:54:31 +0000

--- IntroToOOP/src/main/java/Bird.java
+++ IntroToOOP/src/main/java/Bird.java
@@ -45,14 +45,18 @@ public class Bird {
      *  * greater than or equal to 3.
      */
     public String sound(){
-        if (ageMonths >= 3);{
-        return "Chirp";}
-       
+        if (ageMonths >= 3){
+        return "Chirp";
+    }else{
+        return "Tweet";
+       }
+    }
+}
+
       
-       
-        } 
+
+        
         
 
     
 
-}
