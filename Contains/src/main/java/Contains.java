9ba753f19d48ad629566eeab61d44903a155c37b
VictoriaIvons <victoriaivons@yahoo.com>
2023-01-09 20:13:36 +0000

    Saved progress on Contains 2023-01-09 20:13:36 +0000

--- Contains/src/main/java/Contains.java
+++ Contains/src/main/java/Contains.java
@@ -1,4 +1,4 @@
-
+import kotlin.annotation.Target;
 
 public class Contains {
 
@@ -11,6 +11,11 @@ public class Contains {
      * @return true if arr contains target, false otherwise.
      */
     public boolean arrayContains(int[] arr, int target){
+       for (int i=0; i<arr.length; i++){
+        if (arr[i]==target){
+        return true;
+       }
+}
         return false;
     }
 }
