73f60d3f470631755845b654e1ad0b27625035f3
VictoriaIvons <victoriaivons@yahoo.com>
2023-01-27 22:04:34 +0000

    Saved progress on MostCommonCharacter 2023-01-27 22:04:34 +0000

--- MostCommonCharacter/src/main/java/MostCommonCharacter.java
+++ MostCommonCharacter/src/main/java/MostCommonCharacter.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 
 public class MostCommonCharacter {
     /**
@@ -8,7 +9,14 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        
-        return ' ';
+        HashMap<Character,Integer>map=new HashMap<Character,Integer>();
+         for(int i=0; i<str.length(); i++){
+            if(map.containsKey(str.charAt(i))){
+                map.put(str.charAt(i),map.get(str.charAt(i)+1));
+            }else{
+                map.put(str.charAt(i),1);
+            }
+        }
+        return str;
     }
 }
