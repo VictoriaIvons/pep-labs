dd048c71241e168797a001150f414ac92901d60b
VictoriaIvons <victoriaivons@yahoo.com>
2023-02-01 17:34:07 +0000

    Saved progress on StringIsPalindrome 2023-02-01 17:34:07 +0000

--- StringIsPalindrome/src/main/java/Palindrome.java
+++ StringIsPalindrome/src/main/java/Palindrome.java
@@ -12,6 +12,16 @@ public class Palindrome {
      * @return true if str is a palindrome, false otherwise.
      */
     public boolean pal(String str){
+        String rev=" ";
+      for(int i=str.length()-1;i>0;i--){
+        rev=rev+str.charAt(i);
+       }
+        if(str.equals(rev)){
+        return true;
+       }
         return false;
+        
     }
+ 
 }
+
