2a90ebc23bad0076e9e838d974129ff0d00e3159
VictoriaIvons <victoriaivons@yahoo.com>
2023-02-03 21:18:05 +0000

    Saved progress on FlightTracker 2023-02-03 21:18:05 +0000

--- StringIsPalindrome/src/main/java/Palindrome.java
+++ StringIsPalindrome/src/main/java/Palindrome.java
@@ -13,15 +13,13 @@ public class Palindrome {
      */
     public boolean pal(String str){
         String rev=" ";
-      for(int i=str.length()-1;i>0;i--){
-        rev=rev+str.charAt(i);
-       }
+      for(int i=str.length()-1; i>=0; i--){
+        rev=rev+str.charAt(i);}
         if(str.equals(rev)){
-        return true;
-       }
-        return false;
+        return true; }{
         
-    }
+       }return false;
  
 }
 
+}
