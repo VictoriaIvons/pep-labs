4ccec5c068da94ca037c92b9bc1703455e1f73f8
VictoriaIvons <victoriaivons@yahoo.com>
2023-02-16 22:02:11 +0000

    Saved progress on WordCountMap 2023-02-16 22:02:11 +0000

---
+++ WordCountMap/test-output.sh
@@ -0,0 +1,7 @@
+
+-------------------------------------------------------
+ T E S T S
+-------------------------------------------------------
+Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
+Running WordCountMapTest
+I am a string:
